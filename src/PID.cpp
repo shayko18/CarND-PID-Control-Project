e90524be47d777bc9dd8d21f81fb2dc4071881c6
shayko18 <shaykoren18@gmail.com>
2017-05-23 15:32:23 +0300

    PID.cpp file first commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,23 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+	
+	// set all errors to zero
+	p_error = 0.0;
+	i_error = 0.0;
+	d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+	i_error += cte;         // sum all errors
+	d_error = cte-p_error;  // diff current error from the prev error (p_error) 
+	p_error = cte;          // current error
 }
 
 double PID::TotalError() {
+	return Kp*p_error + Ki*i_error + Kd*d_error;  // sum all errors with their coeff
 }
 
